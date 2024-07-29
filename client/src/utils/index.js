d72728a6020849424b224bf89f204e9bc425da87
yohannis <Abayyohannis2014@gmail.com>
2024-07-29 23:17:03 +0300

    first commit

--- client/src/utils/index.js
+++ client/src/utils/index.js
@@ -1,5 +1,5 @@
 import axios from 'axios';
-const API_URL = 'http:localhost:8800/api-v1';
+const API_URL = 'https://home-tutor-backend.onrender.com';
 export const API = axios.create({
   baseURL: API_URL,
   responseType: 'json',
