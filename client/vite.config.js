a5c481096c2c0f56eab0b35a9d65440da76ef8c3
yohannis <Abayyohannis2014@gmail.com>
2024-07-30 03:01:12 +0300

    first commit

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,10 +1,7 @@
-import react from '@vitejs/plugin-react';
-import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure output directory is set to 'dist'
-  },
-});
+})
